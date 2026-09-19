090ce7be4df891f993eb4f3a254d7a6ca6e258e0
brdmochi13 <brdmochi13@gmail.com>
2026-09-19 14:00:31 +0800

    sync worker.js: short chapter names in radar/chips/QF

--- worker.js
+++ worker.js
@@ -6386,10 +6386,7 @@ function refreshStudyNotes() {
 export default {
   async fetch(request, env) {
     return new Response(HTML, {
-      headers: {
-        'Content-Type': 'text/html;charset=UTF-8',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-      }
+      headers: {'Content-Type':'text/html;charset=UTF-8','Cache-Control':'no-cache'}
     });
   }
 };
