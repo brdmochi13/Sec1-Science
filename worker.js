cb5b64e6dffc0f46f500261a3a16b4fd9f1d4690
brdmochi13 <brdmochi13@gmail.com>
2026-09-20 21:16:57 +0800

    sync worker.js: skill alias fix for radar

--- worker.js
+++ worker.js
@@ -6418,10 +6418,7 @@ function refreshStudyNotes() {
 export default {
   async fetch(request, env) {
     return new Response(HTML, {
-      headers: {
-        'Content-Type': 'text/html;charset=UTF-8',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-      }
+      headers: {'Content-Type':'text/html;charset=UTF-8','Cache-Control':'no-cache'}
     });
   }
 };
